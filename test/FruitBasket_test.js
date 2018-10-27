a0059586230b40a7ca289059f63c3e39dc215580
derrick b <dbozich1@gmail.com>
2018-10-27 23:22:01 +0000

    automatically backed up by learn

--- test/FruitBasket_test.js
+++ test/FruitBasket_test.js
@@ -12,12 +12,12 @@ const Noop = () => (<p>Noop</p>);
 
 // describe('<FruitBasket />', () => {
 //   let wrapper;
-// 
+//
 //   beforeEach(() => {
 //     wrapper = !FruitBasket.prototype ? shallow(<Noop />) :
 //       shallow(<FruitBasket  />);
 //   });
-// 
+//
 //   it('should be a stateless functional component', () => {
 //     const tryToGetState = () => { wrapper.state(); }
 //     expect(tryToGetState).to.throw(
@@ -25,29 +25,29 @@ const Noop = () => (<p>Noop</p>);
 //       'Component should not have state.'
 //     );
 //   });
-// 
+//
 //   it('should have defaultProp "fruit"', () => {
 //     expect(FruitBasket.defaultProps, 'defaultProps not defined.').to.exist;
 //     expect(FruitBasket.defaultProps).to.have.any.key('fruit');
 //   });
-// 
+//
 //   it('should have defaultProp "filters"', () => {
 //     expect(FruitBasket.defaultProps, 'defaultProps not defined.').to.exist;
 //     expect(FruitBasket.defaultProps).to.have.any.key('filters');
 //   });
-// 
+//
 //   it('should have defaultProp "currentFilter"', () => {
 //     expect(FruitBasket.defaultProps, 'defaultProps not defined.').to.exist;
 //     expect(FruitBasket.defaultProps).to.have.any.key('currentFilter');
 //   });
-// 
+//
 //   it('should have defaultProp "updateFilterCallback"', () => {
 //     expect(FruitBasket.defaultProps, 'defaultProps not defined.').to.exist;
 //     expect(FruitBasket.defaultProps).to.have.any.key('updateFilterCallback');
 //   });
-// 
+//
 //   it('should have a top-level div with class "fruit-basket"', () => {
 //     expect(wrapper.find('div').hasClass('fruit-basket')).to.be.true;
 //   });
-// 
+//
 // });
