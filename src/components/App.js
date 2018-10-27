1369e1c9c4013c70c777bdf1d132328f13538ed0
derrick b <dbozich1@gmail.com>
2018-10-27 22:18:59 +0000

    automatically backed up by learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+export default class App extends Component {
 
-export default App;
+  constructor(){
+    super();
+
+    this.state = {
+      filters: [],
+      selectedFilter: 'all',
+      items: []
+
+    }
+  }
+
+    handleFilterChange = event => {
+      console.log('new filter: ', event.target.value);
+      this.setState({ selectedFilter: event.target.value });
+    }
+
+  render(){
+    return(
+      <div>
+        <select onChange={this.props.handleChange} defaultValue='all'>
+            <option value='all'>All</option>
+            {this.state.filters.map(filter =>
+              <option key={filter} value={filter}>{filter}</option>
+            )}
+        </select>
+
+      </div>
+    )
+  }
+
+}
